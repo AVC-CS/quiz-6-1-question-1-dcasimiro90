d48d35e0f615cbafac189630f73628ab1449cc7a
dcasimiro90 <casimiro444@gmail.com>
2024-10-29 01:19:23 -0700

    done

--- main.hpp
+++ main.hpp
@@ -6,3 +6,45 @@
 // ******************************
 // Implement all your functions here
 // ******************************
+
+
+#include <iostream>
+using namespace std;
+
+void getTwoValues(int &begin, int &end);
+int getNextPrime(int num);
+int getPrevPrime(int num);
+int isPrime(int num);
+
+void getTwoValues(int &begin, int &end) {
+    cout << "Enter two integer values (begin < end): ";
+    cin >> begin >> end;
+    while (begin >= end) {
+        cout << "Error: 'begin' must be less than 'end'. Please re-enter values.\n";
+        cin >> begin >> end;
+    }
+}
+
+int getNextPrime(int num) {
+    int nextPrime = num + 1;
+    while (isPrime(nextPrime) == 0) { 
+        nextPrime++;
+    }
+    return nextPrime;
+}
+
+int getPrevPrime(int num) {
+    int prevPrime = num - 1;
+    while (prevPrime > 1 && isPrime(prevPrime) == 0) {
+        prevPrime--;
+    }
+    return prevPrime;
+}
+
+int isPrime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
